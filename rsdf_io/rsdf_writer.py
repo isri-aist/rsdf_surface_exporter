605fcebe51fbfcfe70b745c44c30d904d2ed61aa
ThomasDuvinage <toto.duvinage@gmail.com>
2026-07-10 16:35:53 +0900

    refactor: move XML indent helper into utils/xml_utils

--- rsdf_io/rsdf_writer.py
+++ rsdf_io/rsdf_writer.py
@@ -1,18 +1,7 @@
 import ast
 import xml.etree.ElementTree as ET
 
-
-def indent(elem, level=0):
-    i = "\n" + level * "  "
-    if len(elem):
-        if not elem.text or not elem.text.strip():
-            elem.text = i + "  "
-        for e in elem:
-            indent(e, level + 1)
-        if not e.tail or not e.tail.strip():
-            e.tail = i
-    if level and (not elem.tail or not elem.tail.strip()):
-        elem.tail = i
+from ..utils.xml_utils import indent
 
 
 def export_rsdf(filepath, surfaces):
